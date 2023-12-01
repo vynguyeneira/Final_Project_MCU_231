04bd05c15cfe6b84b79bd1df009db1e1d559403f
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-12-01 16:58:04 +0700

    Add MANUAL MODE

--- Traffic_Light_Project/Core/Inc/fsm_manual_mode.h
+++ Traffic_Light_Project/Core/Inc/fsm_manual_mode.h
@@ -8,6 +8,9 @@
 #ifndef INC_FSM_MANUAL_MODE_H_
 #define INC_FSM_MANUAL_MODE_H_
 
-void fsmAutoModeRun();
+#include "f_global.h"
+#include "main.h"
+
+void fsmManualModeRun();
 
 #endif /* INC_FSM_MANUAL_MODE_H_ */
--- Traffic_Light_Project/Core/Src/fsm_manual_mode.c
+++ Traffic_Light_Project/Core/Src/fsm_manual_mode.c
@@ -7,3 +7,83 @@
 
 #include "fsm_manual_mode.h"
 
+void fsmManualModeRun()
+{
+	switch (mode) {
+		case MANU_MODE:
+			mode = MANU_RED;
+			break;
+
+		case MANU_RED:
+			// Turn on red lights
+			HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, LED_ON);
+			HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, LED_OFF);
+
+			HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, LED_ON);
+			HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, LED_OFF);
+
+			// Switch to MAN_GREEN when press SET button
+			if (isButtonPressed(BUTTON_SET))
+			{
+				mode = MANU_GREEN;
+			}
+
+			// Switch to TUNING MODE when press MODE button
+
+			if(isButtonPressed(BUTTON_MODE))
+			{
+				mode = INC_RED;
+				// something
+			}
+			break;
+
+		case MANU_GREEN:
+			// Turn on red lights
+			HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, LED_OFF);
+			HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, LED_ON);
+
+			HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, LED_OFF);
+			HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, LED_ON);
+
+			// Switch to MAN_YELLOW when press SET button
+			if (isButtonPressed(BUTTON_SET))
+			{
+				mode = MANU_YELLOW;
+			}
+
+			// Switch to TUNING MODE when press MODE button
+
+			if(isButtonPressed(BUTTON_MODE))
+			{
+				mode = INC_GREEN;
+				// something
+			}
+			break;
+
+		case MANU_YELLOW:
+			// Turn on red lights
+			HAL_GPIO_WritePin(LED_RED_1_GPIO_Port, LED_RED_1_Pin, LED_ON);
+			HAL_GPIO_WritePin(LED_GREEN_1_GPIO_Port, LED_GREEN_1_Pin, LED_ON);
+
+			HAL_GPIO_WritePin(LED_RED_2_GPIO_Port, LED_RED_2_Pin, LED_ON);
+			HAL_GPIO_WritePin(LED_GREEN_2_GPIO_Port, LED_GREEN_2_Pin, LED_ON);
+
+			// Switch to MAN_RED when press SET button
+			if (isButtonPressed(BUTTON_SET))
+			{
+				mode = MANU_RED;
+			}
+
+			// Switch to TUNING MODE when press MODE button
+
+			if(isButtonPressed(BUTTON_MODE))
+			{
+				mode = INC_YELLOW;
+				// something
+			}
+			break;
+
+		default:
+			break;
+	}
+}
